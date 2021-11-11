b789ad9ef3892ffa06162ccf791811aae0742157
YUUgUreYUU <549565867@qq.com>
2021-11-11 10:44:25 +0800

    Finish the shipping page and address page

---
+++ Digikey/Address_Info.Designer.cs
@@ -0,0 +1,288 @@
+﻿
+namespace Digikey
+{
+    partial class Address_Info
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TB_FirstName = new System.Windows.Forms.TextBox();
+            this.TB_Address1 = new System.Windows.Forms.TextBox();
+            this.TB_LastName = new System.Windows.Forms.TextBox();
+            this.TB_Address2 = new System.Windows.Forms.TextBox();
+            this.TB_State = new System.Windows.Forms.TextBox();
+            this.TB_PostalCode = new System.Windows.Forms.TextBox();
+            this.TB_City = new System.Windows.Forms.TextBox();
+            this.TB_Phone = new System.Windows.Forms.TextBox();
+            this.TB_Website = new System.Windows.Forms.TextBox();
+            this.TB_Email = new System.Windows.Forms.TextBox();
+            this.TB_ContactInfo = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.Button_ToShip = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TB_FirstName
+            // 
+            this.TB_FirstName.Location = new System.Drawing.Point(160, 109);
+            this.TB_FirstName.Name = "TB_FirstName";
+            this.TB_FirstName.Size = new System.Drawing.Size(79, 20);
+            this.TB_FirstName.TabIndex = 0;
+            this.TB_FirstName.Text = "First Name";
+            this.TB_FirstName.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // TB_Address1
+            // 
+            this.TB_Address1.Location = new System.Drawing.Point(160, 149);
+            this.TB_Address1.Name = "TB_Address1";
+            this.TB_Address1.Size = new System.Drawing.Size(79, 20);
+            this.TB_Address1.TabIndex = 1;
+            this.TB_Address1.Text = "Address Line 1";
+            // 
+            // TB_LastName
+            // 
+            this.TB_LastName.Location = new System.Drawing.Point(160, 129);
+            this.TB_LastName.Name = "TB_LastName";
+            this.TB_LastName.Size = new System.Drawing.Size(79, 20);
+            this.TB_LastName.TabIndex = 2;
+            this.TB_LastName.Text = "Last Name";
+            this.TB_LastName.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // TB_Address2
+            // 
+            this.TB_Address2.Location = new System.Drawing.Point(160, 169);
+            this.TB_Address2.Name = "TB_Address2";
+            this.TB_Address2.Size = new System.Drawing.Size(79, 20);
+            this.TB_Address2.TabIndex = 3;
+            this.TB_Address2.Text = "Address Line 2";
+            // 
+            // TB_State
+            // 
+            this.TB_State.Location = new System.Drawing.Point(160, 209);
+            this.TB_State.Name = "TB_State";
+            this.TB_State.Size = new System.Drawing.Size(79, 20);
+            this.TB_State.TabIndex = 6;
+            this.TB_State.Text = "State";
+            // 
+            // TB_PostalCode
+            // 
+            this.TB_PostalCode.Location = new System.Drawing.Point(160, 229);
+            this.TB_PostalCode.Name = "TB_PostalCode";
+            this.TB_PostalCode.Size = new System.Drawing.Size(79, 20);
+            this.TB_PostalCode.TabIndex = 5;
+            this.TB_PostalCode.Text = "Postal Code";
+            // 
+            // TB_City
+            // 
+            this.TB_City.Location = new System.Drawing.Point(160, 189);
+            this.TB_City.Name = "TB_City";
+            this.TB_City.Size = new System.Drawing.Size(79, 20);
+            this.TB_City.TabIndex = 4;
+            this.TB_City.Text = "City";
+            // 
+            // TB_Phone
+            // 
+            this.TB_Phone.Location = new System.Drawing.Point(160, 301);
+            this.TB_Phone.Name = "TB_Phone";
+            this.TB_Phone.Size = new System.Drawing.Size(79, 20);
+            this.TB_Phone.TabIndex = 10;
+            this.TB_Phone.Text = "Telephone";
+            // 
+            // TB_Website
+            // 
+            this.TB_Website.Location = new System.Drawing.Point(160, 321);
+            this.TB_Website.Name = "TB_Website";
+            this.TB_Website.Size = new System.Drawing.Size(79, 20);
+            this.TB_Website.TabIndex = 9;
+            this.TB_Website.Text = "Website";
+            // 
+            // TB_Email
+            // 
+            this.TB_Email.Location = new System.Drawing.Point(160, 281);
+            this.TB_Email.Name = "TB_Email";
+            this.TB_Email.Size = new System.Drawing.Size(79, 20);
+            this.TB_Email.TabIndex = 8;
+            this.TB_Email.Text = "Email";
+            // 
+            // TB_ContactInfo
+            // 
+            this.TB_ContactInfo.Location = new System.Drawing.Point(160, 255);
+            this.TB_ContactInfo.Name = "TB_ContactInfo";
+            this.TB_ContactInfo.Size = new System.Drawing.Size(299, 20);
+            this.TB_ContactInfo.TabIndex = 7;
+            this.TB_ContactInfo.Text = "Your Contact Information";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(245, 209);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 20);
+            this.textBox1.TabIndex = 17;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(245, 229);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(214, 20);
+            this.textBox2.TabIndex = 16;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(245, 189);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(214, 20);
+            this.textBox3.TabIndex = 15;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(245, 169);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(214, 20);
+            this.textBox4.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(245, 129);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(214, 20);
+            this.textBox5.TabIndex = 13;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(245, 149);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(214, 20);
+            this.textBox6.TabIndex = 12;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(245, 109);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(214, 20);
+            this.textBox7.TabIndex = 11;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(245, 301);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(214, 20);
+            this.textBox8.TabIndex = 20;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(245, 321);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(214, 20);
+            this.textBox9.TabIndex = 19;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(245, 281);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(214, 20);
+            this.textBox10.TabIndex = 18;
+            // 
+            // Button_ToShip
+            // 
+            this.Button_ToShip.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Button_ToShip.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button_ToShip.Location = new System.Drawing.Point(831, 429);
+            this.Button_ToShip.Name = "Button_ToShip";
+            this.Button_ToShip.Size = new System.Drawing.Size(313, 80);
+            this.Button_ToShip.TabIndex = 21;
+            this.Button_ToShip.Text = "Continue to Shipping";
+            this.Button_ToShip.UseVisualStyleBackColor = false;
+            // 
+            // Address_Info
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1884, 711);
+            this.Controls.Add(this.Button_ToShip);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.TB_Phone);
+            this.Controls.Add(this.TB_Website);
+            this.Controls.Add(this.TB_Email);
+            this.Controls.Add(this.TB_ContactInfo);
+            this.Controls.Add(this.TB_State);
+            this.Controls.Add(this.TB_PostalCode);
+            this.Controls.Add(this.TB_City);
+            this.Controls.Add(this.TB_Address2);
+            this.Controls.Add(this.TB_LastName);
+            this.Controls.Add(this.TB_Address1);
+            this.Controls.Add(this.TB_FirstName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Address_Info";
+            this.Text = "Address Infomation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TB_FirstName;
+        private System.Windows.Forms.TextBox TB_Address1;
+        private System.Windows.Forms.TextBox TB_LastName;
+        private System.Windows.Forms.TextBox TB_Address2;
+        private System.Windows.Forms.TextBox TB_State;
+        private System.Windows.Forms.TextBox TB_PostalCode;
+        private System.Windows.Forms.TextBox TB_City;
+        private System.Windows.Forms.TextBox TB_Phone;
+        private System.Windows.Forms.TextBox TB_Website;
+        private System.Windows.Forms.TextBox TB_Email;
+        private System.Windows.Forms.TextBox TB_ContactInfo;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Button Button_ToShip;
+    }
+}
---
+++ Digikey/Address_Info.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Digikey
+{
+    public partial class Address_Info : Form
+    {
+        public Address_Info()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
